904d561e76357a01874361cf2d927b9251a180de
HoloborodkoM <mykhailoholoborodko@gmail.com>
2023-12-12 03:22:41 +0200

    lab4: add recolor for 1 and 2 elements

--- lab4/src/script.js
+++ lab4/src/script.js
@@ -1,2 +1,22 @@
 const getFirstElement = document.getElementById('first-element');
 const getSecondElement = document.querySelector('#second-element');
+
+const getRandomColor = () => {
+   const possibleLetters = '0123456789ABCDEF';
+   let color = '#';
+
+   for(let i = 0; i < 6; i++) {
+      color += possibleLetters[Math.floor(Math.random() * 16)];
+   }
+   return color;
+}
+
+const changeColor = (element) => {
+   const changedBackgroundColor = getRandomColor();
+   const changedTextColor = getRandomColor();
+   element.style.backgroundColor = changedBackgroundColor;
+   element.style.color = changedTextColor;
+}
+
+getFirstElement.onclick = () => changeColor(getFirstElement);
+getSecondElement.onclick = () => changeColor(getSecondElement);
