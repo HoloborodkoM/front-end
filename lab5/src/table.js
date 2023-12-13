5181f242804ca7cd1e04575ca55bd9a9ef358572
HoloborodkoM <mykhailoholoborodko@gmail.com>
2023-12-13 15:08:48 +0200

    lab5: add table basic skeleton with styles

---
+++ lab5/src/table.js
@@ -0,0 +1,20 @@
+const tableSize = 6;
+let value = 0;
+
+const getTableElement = document.querySelector('#table-six-x-six');
+
+for (let i = 0; i < tableSize; i++) {
+   
+   const tableRow = document.createElement('tr');
+   getTableElement.append(tableRow);
+
+   for (let j = 0; j < tableSize; j++){
+
+      ++value;
+      const tableCell = document.createElement('td');
+      tableCell.textContent = value;
+      tableCell.id = `cell-${value}`;
+      tableRow.append(tableCell);
+
+   }
+}
