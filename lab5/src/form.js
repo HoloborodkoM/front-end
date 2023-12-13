62b0bc232f0207c4edc8a98623afcba4480af40f
HoloborodkoM <mykhailoholoborodko@gmail.com>
2023-12-13 14:10:08 +0200

    lab5: add regular expressions for input elements

--- lab5/src/form.js
+++ lab5/src/form.js
@@ -5,3 +5,19 @@ const formInputElements = {
    formFaculty: document.getElementById('faculty'),
    formBirthDate: document.getElementById('birth-date'),
 }
+
+const ukrainianPattern = {
+   capitalLetters: '[А-ЩЬЮЯҐЄІЇ]',
+   smallLetters: '[а-щьюяґєії]',
+}
+
+const formRegularExpressions = {
+   formNamePattern: new RegExp(
+      `^` + 
+      `(${ukrainianPattern.capitalLetters}|${ukrainianPattern.capitalLetters}${ukrainianPattern.smallLetters}+)` +
+      ` ${ukrainianPattern.capitalLetters}\\.${ukrainianPattern.capitalLetters}\\.$`),
+   formPhonePattern: new RegExp('^\\(\\d{3}\\)-\\d{3}-\\d{2}-\\d{2}$'),
+   formIdCardPattern: new RegExp(`^${ukrainianPattern.capitalLetters}{2} №\\d{6}$`),
+   formFacultyPattern: new RegExp(`^${ukrainianPattern.capitalLetters}{4}$`),
+   formBirthDatePattern: new RegExp('^(0[1-9]|[12]\\d|3[01])\\.(0[1-9]|1[0-2])\\.(19[2-9]\\d|2[01]\\d{2})$'),
+}
