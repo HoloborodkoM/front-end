c982de237ffc5878d3f806de233e8af71af829da
HoloborodkoM <mykhailoholoborodko@gmail.com>
2023-12-13 14:07:40 +0200

    lab5: add collection input elements

---
+++ lab5/src/form.js
@@ -0,0 +1,7 @@
+const formInputElements = {
+   formName: document.getElementById('name'),
+   formPhone: document.getElementById('phone'),
+   formIdCard: document.getElementById('id-card'),
+   formFaculty: document.getElementById('faculty'),
+   formBirthDate: document.getElementById('birth-date'),
+}
