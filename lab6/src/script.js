f360d70b1e2d04dccaca11b62036b86fed03bf28
HoloborodkoM <mykhailoholoborodko@gmail.com>
2023-12-15 03:10:07 +0200

    lab6: add event listerer on click button and create request function to get data

--- lab6/src/script.js
+++ lab6/src/script.js
@@ -1,3 +1,27 @@
 const downloadButton = document.getElementById('get-users-info');
 const outputUsers = document.getElementById('output');
 const requestInfo = document.querySelector('.hidden-info');
+
+const getUsers = () => {
+
+   requestInfo.classList.remove('hidden-info');
+   let url = new URL('https://randomuser.me/api');
+   url.searchParams.append('results', 5);
+
+   fetch(url)
+   .then(res => {
+      if (res.ok) {
+         requestInfo.textContent = "Успішне з'єднання! Дані отримано";
+         return res.json();
+      } else {
+         requestInfo.textContent = "Упс... Щось пішло не так!";
+         throw new Error('Fail request');
+      }
+   })
+   .then(data => fillUsersInfo(data.results))
+   .catch(error => {
+      console.log(error);
+   })
+}
+
+downloadButton.addEventListener('click', () => getUsers());
