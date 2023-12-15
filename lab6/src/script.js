86eed57aff31baeb8069eca7b749d9eaadf4b188
HoloborodkoM <mykhailoholoborodko@gmail.com>
2023-12-15 03:05:43 +0200

    lab6: get elements from document

---
+++ lab6/src/script.js
@@ -0,0 +1,3 @@
+const downloadButton = document.getElementById('get-users-info');
+const outputUsers = document.getElementById('output');
+const requestInfo = document.querySelector('.hidden-info');
